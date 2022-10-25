1ffb4eea4f23524241b2bc0c024fbe87797fdc43
CarlosEduardo-galleth <carlospcavalari@gmail.com>
2022-10-25 19:03:53 -0300

    add functions

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,96 @@
-/* Desenvolva seu script aqui */
+
+const baseUrl = "https://m2-api-living.herokuapp.com"
+
+async function buscarPost() {
+    await fetch(`${baseUrl}/news?page=1`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((response) => renderizarPost(response.news))
+}
+function renderizarPost(post) {
+    post.forEach(element => {
+        let postsList = document.getElementsByClassName("container")[0]
+
+        let li = document.createElement("li")
+        li.classList = "post flex flex-col"
+
+        let postImg = document.createElement("img")
+        postImg.src = element.image
+
+        let divInfo = document.createElement("div")
+        let h3Title = document.createElement("h3")
+        let pDescription = document.createElement("p")
+        let accessContent = document.createElement("a")
+
+        divInfo.classList = "post-info flex flex-col"
+        h3Title.innerText = element.title
+        pDescription.innerText = element.description
+        accessContent.innerText = "Acessar conteúdo"
+        accessContent.href = "../post/index.html"
+        accessContent.target = "_blank"
+        divInfo.append(h3Title, pDescription, accessContent)
+        li.append(postImg, divInfo)
+        postsList.append(li)
+
+        accessContent.addEventListener('click', () => {
+            let strPost = JSON.stringify(element)
+            localStorage.setItem("post", strPost)
+        })
+    });
+}
+
+function filtrarCategoria() {
+    let postsList = document.getElementsByClassName("container")[0]
+    let post = document.querySelectorAll("#postC")
+    post.forEach((elem) => {
+        elem.addEventListener('click', async () => {
+            postsList.innerHTML = ""
+            post.forEach((btn) => {
+                btn.classList.remove("choosed-btn")
+            })
+            elem.classList.add("choosed-btn")
+
+            await fetch(`${baseUrl}/news?page=1`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((response) => response.json())
+            .then((response) => filtrar(response.news, elem))
+        })
+    })
+}
+filtrarCategoria()
+function filtrar(post, elem) {
+    let arrFiltered = []
+    post.forEach((element) => {
+        if (element.category == elem.innerText) {
+            arrFiltered.push(element)
+        }
+        if (elem.innerText == "Todos") {
+            arrFiltered.push(element)
+        }
+    })
+    renderizarPost(arrFiltered)
+}
+
+function irAoConteudo() {
+    let goContent = document.getElementById("go-to-content")
+    goContent.addEventListener('click', () =>{
+        window.scrollTo(0, 580)
+    })
+}
+irAoConteudo()
+function voltarAoTopo() {
+    let backTop = document.getElementById("back-to-top")
+    backTop.addEventListener('click', () =>{
+        window.scrollTo(0, 0)
+    })
+}
+voltarAoTopo()
+buscarPost()
--- pages/post/index.js
+++ pages/post/index.js
@@ -1,1 +1,31 @@
-/* Desenvolva seu script aqui */
+
+async function renderizarPost() {
+    let post = localStorage.getItem("post")
+    let postJS = JSON.parse(post)
+    let postTitle = document.getElementById("post-title")
+    let postDescription = document.getElementById("post-description")
+    let postImg = document.getElementById("post-img")
+    let postContent = document.getElementById("post-content")
+
+    postTitle.innerText = postJS.title
+    postDescription.innerText = postJS.description
+    postImg.src = postJS.image
+    postContent.innerText = postJS.content
+}
+
+function retornar() {
+    let returnBtn = document.getElementById("return")
+    returnBtn.addEventListener('click', () => {
+        window.location.assign("../home/index.html")
+    })
+}
+retornar()
+
+function voltarAoTopo() {
+    let backTop = document.getElementById("back-to-top")
+    backTop.addEventListener('click', () =>{
+        window.scrollTo(0, 0)
+    })
+}
+voltarAoTopo()
+renderizarPost()
